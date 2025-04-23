c854c61fe1df08c8e2e51b0102ddbc83df9a78d7
Bhumika <bhumikaspandana410@gmail.com>
2025-04-23 23:50:56 +0530

    fetching data from real api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,7 @@
 import Restocard from "./Restocard";
 import {restoList} from '../utils/mockData';
 import { useState } from "react";
+import { useEffect } from "react";
 
 
 
@@ -16,14 +17,23 @@ const Body=()=>{
         })
         setTopRated(()=>List);
     }
-    function showall(){
-        setTopRated(()=>restoList);
+
+    useEffect(()=>{
+        name();
+    },[])
+
+    const name=async() =>{
+        const response= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.97530&lng=77.59100&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const mod= await response.json();
+    console.log(mod);
     }
+    
+    
     return(
       <>
       {/* <div className='search'>Search</div> */}
       <button onClick={topRatedResto}>Top rated resto</button>
-      <button onClick={showall}>all resto</button>
+
       <div className='resto-container'>
       {topRated.map((item)=>{
         return <Restocard restoProp={item}></Restocard>
